9af4a3773fdc8a2cc2d7ca5523b514a81e1901b7
Nils Niggemann <nils.niggemann@fu-berlin.de>
2022-10-31 12:15:14 +0100

    added methods from FRGLatticePlots

--- src/FRGLatticeEvaluation.jl
+++ src/FRGLatticeEvaluation.jl
@@ -1,5 +1,14 @@
 module FRGLatticeEvaluation
+    
+    using SpinFRGLattices,StaticArrays,LaTeXStrings
 
-greet() = print("Hello World!")
+    using SLEEFPirates:cos_fast
+
+    include("Fourier.jl")
+    export AbstractLattice, LatticeInfo,FourierTransform, Fourier2D,  getFlow, getMaxFlow, pointPath, fetchKPath,getkMax,Fourier3D
+        
+    include("helpers.jl")
+    export hhlplane,normalTohhlplane,xyplane,zzerocut,sphereplane,hhllabels,xylabels,zzerolabels,normalTohhllabels,strd,strd3,getPairs,DimerResponse,DimerResponse_Keles,getFourier
+
 
 end # module FRGLatticeEvaluation
