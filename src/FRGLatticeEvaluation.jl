8faa05a1a3ddea7ca475300b31b4926608163301
Nils Niggemann <nils.niggemann@fu-berlin.de>
2022-11-03 11:17:16 +0100

    exported Correlation length

--- src/FRGLatticeEvaluation.jl
+++ src/FRGLatticeEvaluation.jl
@@ -5,7 +5,7 @@ module FRGLatticeEvaluation
     using SLEEFPirates:cos_fast
 
     include("Fourier.jl")
-    export AbstractLattice, LatticeInfo,FourierTransform, Fourier2D,  getFlow, getMaxFlow, pointPath, fetchKPath,getkMax,Fourier3D
+    export AbstractLattice, LatticeInfo,FourierTransform, Fourier2D,Fourier3D,  getFlow, getMaxFlow, pointPath, fetchKPath,getkMax,CorrelationLength
         
     include("helpers.jl")
     export hhlplane,normalTohhlplane,xyplane,zzerocut,sphereplane,hhllabels,xylabels,zzerolabels,normalTohhllabels,strd,strd3,getPairs,DimerResponse,DimerResponse_Keles,getFourier
